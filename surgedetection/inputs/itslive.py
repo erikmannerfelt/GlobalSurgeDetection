d346dc22965b89d3eba68ed5dde4e5205a4d09d6
Erik Mannerfelt <33550973+erikmannerfelt@users.noreply.github.com>
2023-01-24 13:03:51 +0100

    Incremental commit on downloading raw ITS-LIVE tiles for mosaicking

--- surgedetection/inputs/itslive.py
+++ surgedetection/inputs/itslive.py
@@ -3,6 +3,9 @@ import warnings
 from pathlib import Path
 import asyncio
 import shutil
+import tempfile
+import time
+import os
 
 import pandas as pd
 import rasterio as rio
@@ -67,7 +70,7 @@ def get_files(region: pd.Series) -> list[RasterInput]:
             )
         )
 
-    return rasters
+    return rasters + get_tiles(region=region)
 
 
 def rgi_mapping(rgi_region: int | str, res: str = "_G0240") -> str | None:
@@ -250,7 +253,24 @@ def process_raw_tiles(data: xr.Dataset,out_filepath: Path, region: pd.Series | s
 
     print(out)
 
-def get_tiles(region: pd.Series | str = "REGN79E021X24Y05"):
+
+def get_all_tiles():
+
+    regions = surgedetection.regions.make_glacier_regions()
+
+    for _, region in tqdm(regions.iterrows(), desc="Downloading its-live tiles", total=regions.shape[0]):
+
+        rgi_regions = map(int, region["O1_regions"].split("/"))
+
+        if any((rgi_mapping(reg) is None for reg in rgi_regions)):
+            min_year = 0
+        else:
+            min_year = 2019 
+
+        get_tiles(region=region, min_year=min_year, progress=False)
+
+
+def get_tiles(region: pd.Series | str = "REGN79E021X24Y05", min_year: int = 2019, progress: bool = True):
     warnings.simplefilter("error")
 
     if isinstance(region, str):
@@ -258,40 +278,120 @@ def get_tiles(region: pd.Series | str = "REGN79E021X24Y05"):
 
     tile_meta = get_tile_metadata()
     tile_meta = tile_meta[tile_meta.intersects(region.geometry) | tile_meta.overlaps(region.geometry)]
-    final_cache_path = surgedetection.cache.get_cache_name(f"itslive-get_tiles/yearly_mosaics-{region['region_id']}", args=tile_meta["zarr_url"], extension="zarr")
 
     if tile_meta.shape[0] == 0:
         return []
 
-    tiles = []
+    finished = []
 
     not_finished = []
     for _, tile_m in tile_meta.iterrows():
-        cache_path = surgedetection.cache.get_cache_name(f"itslive-get_tiles/yearly_mean-{region['region_id']}", args=tile_m["zarr_url"], extension="zarr")
+        cache_path = surgedetection.cache.get_cache_name(f"itslive-get_tiles/yearly_mean-" + tile_m["zarr_url"].split("/")[-1].replace(".zarr", ""), extension="zarr")
         if cache_path.is_dir():
-            tiles.append(cache_path)
+            finished.append((cache_path, tile_m))
         else:
             not_finished.append((cache_path, tile_m))
             
     
-    for cache_path, tile_m in tqdm(not_finished, desc=f"Downloading ITS-LIVE data for region {region['region_id']}", disable=len(not_finished) < 2):
-        cache_path = surgedetection.cache.get_cache_name(f"itslive-get_tiles/yearly_mean-{region['region_id']}", args=tile_m["zarr_url"], extension="zarr")
-
+    for cache_path, tile_m in tqdm(not_finished, desc=f"Downloading ITS-LIVE data for region {region['region_id']}", disable=(len(not_finished) < 2) or not progress):
         if not cache_path.is_dir():
             tile = xr.open_zarr(tile_m["zarr_url"])[["v", "v_error"]]
 
-            tile2 = tile.groupby("mid_date.year").map(lambda df: df.weighted(xr.apply_ufunc(np.reciprocal, df["v_error"].where(df["v_error"] > 0), dask="allowed").fillna(0)).mean("mid_date"))
+            tile["weight"] = (1000. - tile["v_error"]).fillna(0)
+
+            #tile2 = tile.groupby("mid_date.year").map(lambda df: df.weighted(xr.apply_ufunc(np.reciprocal, df["v_error"].where(df["v_error"] > 0), dask="allowed").fillna(0)).mean("mid_date"))
+            tile2 = tile.groupby("mid_date.year").map(lambda df: df.weighted(df["weight"]).mean("mid_date", skipna=True)).drop_vars(["weight"]).sel(year=slice(min_year, None))
             tile2.attrs = tile.attrs.copy()
             for variable in tile.data_vars:
+                if variable not in tile2.data_vars:
+                    continue
                 tile2[variable].attrs = tile[variable].attrs.copy()
         
-            task = tile2.to_zarr(cache_path, encoding={v: {"compressor": zarr.Blosc(cname="zstd", clevel=3, shuffle=2)} for v in tile2.data_vars}, compute=False)
-            with TqdmCallback(desc=f"Calculating {cache_path.stem}", disable=len(not_finished) > 1), warnings.catch_warnings():
-                warnings.filterwarnings("ignore", message="All-NaN slice encountered") 
-                warnings.filterwarnings("ignore", message="divide by zero encountered") 
+            with tempfile.TemporaryDirectory() as temp_dir_str:
+                temp_path = Path(temp_dir_str).joinpath("arr.zarr") 
+                task = tile2.to_zarr(temp_path, encoding={v: {"compressor": zarr.Blosc(cname="zstd", clevel=3, shuffle=2)} for v in tile2.data_vars}, compute=False)
+                with TqdmCallback(desc=f"Calculating {cache_path.stem}", disable=(len(not_finished) > 1) or not progress), warnings.catch_warnings():
+                    warnings.filterwarnings("ignore", message="All-NaN slice encountered") 
+                    warnings.filterwarnings("ignore", message="divide by zero encountered") 
                 task.compute()
-        tiles.append(cache_path)
 
+                shutil.move(temp_path,cache_path) 
+        finished.append((cache_path, tile_m))
+
+    tiles = {"v": {}, "v_error": {}}
+
+    for cache_path, tile_m in finished:
+        data = xr.open_zarr(cache_path)
+
+        for variable in tiles:
+            if tile_m["epsg"] not in tiles[variable]:
+                tiles[variable][tile_m["epsg"]] = {}
+
+            for i, year in enumerate(data["year"].values):
+                if year not in tiles[variable][tile_m["epsg"]]:
+                    tiles[variable][tile_m["epsg"]][year] = []                    
+
+                tiles[variable][tile_m["epsg"]][year].append(f"ZARR:{cache_path}/{variable}:/{variable}:{i}")
+
+
+    tile_cache_dir = surgedetection.cache.get_cache_name(f"itslive-get_tiles/tile_vrts-{region['region_id']}", args=tile_meta["zarr_url"])
+    os.makedirs(tile_cache_dir, exist_ok=True)
+    with tempfile.TemporaryDirectory() as temp_dir_str:
+        
+        temp_dir = Path(temp_dir_str)
+        temp_dir = tile_cache_dir
+
+        per_epsg = {}
+        for variable in tiles:
+            for epsg in tiles[variable]:
+                for year in tiles[variable][epsg]:
+                    if variable not in per_epsg:
+                        per_epsg[variable] = {}
+                    if year not in per_epsg[variable]:
+                        per_epsg[variable][year] = []
+                    temp_path = temp_dir.joinpath(f"{variable}-{year}-{epsg}.vrt")
+                    temp_path2 = temp_path.with_stem(temp_path.stem + "_reproj")
+                    surgedetection.rasters.build_vrt(tiles[variable][epsg][year], vrt_filepath=temp_path)
+                    surgedetection.rasters.build_vrt_new(temp_path, vrt_filepath=temp_path2,  src_crs=f"epsg:{epsg}", dst_crs="epsg:32633")
+
+
+                    per_epsg[variable][year].append(temp_path2)
+
+        mosaics = []
+        #mosaics = {}
+        for variable in per_epsg:
+            #if variable not in mosaics:
+            #    mosaics[variable] = {}
+            for year in per_epsg[variable]:
+                if year < min_year:
+                    continue
+                temp_path = temp_dir.joinpath(f"{variable}-{year}.vrt")
+
+                surgedetection.rasters.build_vrt_new(
+                    per_epsg[variable][year],
+                    vrt_filepath=temp_path,
+                    dst_bounds=region[["xmin_proj", "ymin_proj", "xmax_proj", "ymax_proj"]],
+                    dst_res=CONSTANTS.pixel_size,
+                )
+
+                with rio.open(temp_path) as raster:
+                    assert raster.crs is not None
+
+                #mosaics[variable][year] = temp_path
+                mosaics.append(
+                    surgedetection.rasters.RasterInput(
+                        sources="its-live",
+                        start_dates= pd.Timestamp(f"{year}-01-01"),
+                        end_dates=pd.Timestamp(f"{year}-01-01"),
+                        kinds="ice_velocity" if variable == "v" else "ice_velocity_err",
+                        region=region["region_id"],
+                        filepath=temp_path,
+                        multi_date=True
+                    )
+                )
+
+
+    return mosaics
 
     print("Opening dataset" + surgedetection.utilities.now_str())
     with warnings.catch_warnings():
